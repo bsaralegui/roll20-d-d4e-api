2226307fcc9bacc70e6b21b9fb77fd2c01349fa7
Benjamin Saralegui <bsaralegui@gmail.com>
2024-06-13 21:03:56 -0600

    Convert to class
    This commit updates the object to a class.
    
    This was done to allow extending to other actions.

--- chat_actions.js
+++ chat_actions.js
@@ -1,15 +1,50 @@
-var ChatActions = {
+/**
+ * A collection of Roll20 Chat Actions
+ * @module ChatActions
+ * @class ChatActions
+ *
+ * The following actions are available:
+ * - sendChat - Sends a chat message
+ * - sendChatFromPlayer - Sends a chat message as a player
+ * - sendChatToGM - Sends a chat message to the GM
+ */
+class ChatActions {
+
 	// Add properties here for your actions
-	gmWhisperPrefix: "/w gm ",
-	systemSender: "System",
-	sendChat: function(message, sender) {
-		if(sender) {
-			sendChat(sender, message);
-		} else {
-			sendChat(this.systemSender, message);
-		}
-	},
-	sendGmChat: function(message, sender) {
+	constructor() {
+		// Add properties here for your actions
+		this.gmWhisperPrefix = "/w gm ";
+		this.systemSender = "System";
+	}
+
+	/**
+	 * Sends a chat message.
+	 *
+	 * @param {string} message - The message to be sent
+	 * @param {string} sender - Optional. The sender of the message. Defaults to "System"
+	 */
+	sendChat(message, sender = this.systemSender) {
+		sendChat(this.systemSender, message);
+	}
+
+	/**
+	 * Sends a chat message as a player.
+	 *
+	 * @param {string} message - The message to be sent
+	 * @param {string} playerid - The player ID for sending the message
+	 */
+	sendChatFromPlayer(message, playerid) {
+		// Implementation for sending a chat message as a player
+		sendChat(`player|${playerid}`, message);
+	}
+
+	/**
+	 * Sends a chat message to the GM
+	 *
+	 * @param {string} message - The message to be sent
+	 * @param {string} sender - Optional. The sender of the message. Defaults to "System"
+	 */
+	sendGmChat(message, sender) {
 		this.sendChat(`${this.gmWhisperPrefix} {message}`, sender);
 	}
-};
+}
